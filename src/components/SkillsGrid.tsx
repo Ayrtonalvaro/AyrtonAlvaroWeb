ed2ade2d2ef2301ebbcb9bc54018d2bdcb1dca0d
Ayrtonalvaro <ayrtonalvaro1973@gmail.com>
2023-02-12 13:14:19 -0300

    add animation grid skill

--- src/components/SkillsGrid.tsx
+++ src/components/SkillsGrid.tsx
@@ -7,39 +7,31 @@ import react from '../assets/SkillsIcon/tinyreact.png'
 import spring from '../assets/SkillsIcon/tinyspring.png'
 import ts from '../assets/SkillsIcon/tinyts.png'
 import tailwind from '../assets/SkillsIcon/tinytailwind.png'
-
+import { motion } from 'framer-motion'
 const SkillsGrid = () => {
   return (
-    <div>
-      <div className="mt-5 md:mt-10 2xl:mt-36">
-        <div className="max-xl:flex  max-xl:items-center max-xl:justify-center max-xl:gap-5 2xl:flex 2xl:justify-center 2xl:items-center 2xl:gap-5">
-          <SkillIcon scrSkill={tailwind} linkSkill="https://tailwindcss.com/" />
-        </div>
-        <div className="max-2xl:flex xl:gap-10 max-2xl:items-center max-2xl:justify-center max-xl:gap-5 2xl:flex 2xl:justify-center 2xl:items-center 2xl:gap-5">
-          <SkillIcon
-            scrSkill={bootstrap}
-            linkSkill="https://getbootstrap.com/"
-          />
-          <SkillIcon
-            scrSkill={css}
-            linkSkill="https://developer.mozilla.org/es/docs/Web/CSS"
-          />
-          <SkillIcon
-            scrSkill={ts}
-            linkSkill="https://www.typescriptlang.org/"
-          />
-        </div>
-        <div className="max-xl:flex xl:flex xl:items-center xl:justify-center xl:gap-10 max-xl:items-center max-xl:justify-center max-xl:gap-5 2xl:flex 2xl:justify-center 2xl:items-center 2xl:gap-5">
-          <SkillIcon scrSkill={java} linkSkill="https://www.java.com/es/" />
-          <SkillIcon
-            scrSkill={javascript}
-            linkSkill="https://www.javascript.com/"
-          />
-          <SkillIcon scrSkill={react} linkSkill="https://es.reactjs.org/" />
-        </div>
-        <div className="max-xl:flex  max-xl:items-center max-xl:justify-center 2xl:flex 2xl:justify-center 2xl:items-center 2xl:gap-5">
-          <SkillIcon scrSkill={spring} linkSkill="https://spring.io/" />
-        </div>
+    <div className="mt-5 md:mt-10 2xl:mt-36">
+      <div className="max-xl:flex  max-xl:items-center max-xl:justify-center max-xl:gap-5 2xl:flex 2xl:justify-center 2xl:items-center 2xl:gap-5">
+        <SkillIcon scrSkill={tailwind} linkSkill="https://tailwindcss.com/" />
+      </div>
+      <div className="max-2xl:flex xl:gap-10 max-2xl:items-center max-2xl:justify-center max-xl:gap-5 2xl:flex 2xl:justify-center 2xl:items-center 2xl:gap-5">
+        <SkillIcon scrSkill={bootstrap} linkSkill="https://getbootstrap.com/" />
+        <SkillIcon
+          scrSkill={css}
+          linkSkill="https://developer.mozilla.org/es/docs/Web/CSS"
+        />
+        <SkillIcon scrSkill={ts} linkSkill="https://www.typescriptlang.org/" />
+      </div>
+      <div className="max-xl:flex xl:flex xl:items-center xl:justify-center xl:gap-10 max-xl:items-center max-xl:justify-center max-xl:gap-5 2xl:flex 2xl:justify-center 2xl:items-center 2xl:gap-5">
+        <SkillIcon scrSkill={java} linkSkill="https://www.java.com/es/" />
+        <SkillIcon
+          scrSkill={javascript}
+          linkSkill="https://www.javascript.com/"
+        />
+        <SkillIcon scrSkill={react} linkSkill="https://es.reactjs.org/" />
+      </div>
+      <div className="max-xl:flex  max-xl:items-center max-xl:justify-center 2xl:flex 2xl:justify-center 2xl:items-center 2xl:gap-5">
+        <SkillIcon scrSkill={spring} linkSkill="https://spring.io/" />
       </div>
     </div>
   )
--- src/main/Skills.tsx
+++ src/main/Skills.tsx
@@ -5,19 +5,29 @@ import SkillsGrid from '../components/SkillsGrid';
 const Skills = () => {
   return (
     <div className="2xl:absolute 2xl:top-0 2xl:right-72">
+      <h2 className="dark:text-cyan-500 text-blue-600 text-center max-sm:text-5xl text-6xl   pt-10 2xl:hidden z-1">
+        Skills
+      </h2>
       <motion.div
-        className="z-1"
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        transition={{ delay: 0.1 }}
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          default: {
+            duration: 0.3,
+            ease: [0, 0.71, 0.2, 1.01]
+          },
+          scale: {
+            type: 'spring',
+            damping: 5,
+            stiffness: 100,
+            restDelta: 0.001
+          }
+        }}
       >
-        <h2 className="dark:text-cyan-500 text-blue-600 text-center max-sm:text-5xl text-6xl   pt-10 2xl:hidden ">
-          Skills
-        </h2>
         <SkillsGrid />
       </motion.div>
     </div>
-  );
+  )
 };
 
 export default Skills;
